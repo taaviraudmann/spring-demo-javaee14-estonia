2bf265b54c2f0e04d23029531931c336607caf73
taaviraudmann <taaviraudmann@gmail.com>
2022-06-18 11:06:15 +0300

    [init] - project skeleton provided

---
+++ src/main/java/com/sda/springdemojavaee14/SpringDemoJavaee14Application.java
@@ -0,0 +1,13 @@
+package com.sda.springdemojavaee14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoJavaee14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoJavaee14Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/springdemojavaee14/SpringDemoJavaee14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.springdemojavaee14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoJavaee14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
