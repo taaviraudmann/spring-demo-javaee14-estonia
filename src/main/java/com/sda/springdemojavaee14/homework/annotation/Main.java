ceed0a99257cffd1ea3829357138bc4037e31516
taaviraudmann <taaviraudmann@gmail.com>
2022-07-10 11:16:24 +0300

    [add] - Person with builder

--- src/main/java/com/sda/springdemojavaee14/homework/annotation/Main.java
+++ src/main/java/com/sda/springdemojavaee14/homework/annotation/Main.java
@@ -18,5 +18,13 @@ public class Main {
         System.out.println("is this the same person?-" + me.equals(anotherMe));
 
         System.out.println("are equals: " + me.equals("haha"));
+
+        Person meAgain = new Person("Taavi", "Raudmann", 35);
+        //Builder is better than all args constructor - because you can see more :)
+        Person meAgainWithBuilder = Person.builder()
+                .age(35)
+                .surname("Raudmann")
+                .name("Taavi")
+                .build();
     }
 }
--- src/main/java/com/sda/springdemojavaee14/homework/annotation/Person.java
+++ src/main/java/com/sda/springdemojavaee14/homework/annotation/Person.java
@@ -1,12 +1,14 @@
 package com.sda.springdemojavaee14.homework.annotation;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data // generates all getters/setter, toString(), equals(), hashcode()
 @AllArgsConstructor // generates constructor for all fields in order of their declaration
 @NoArgsConstructor // generates default constructor
+@Builder //
 public class Person {
 
     private String name;
