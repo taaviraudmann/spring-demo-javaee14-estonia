9155d67a5b20545efdba0fefb83d038872aaa59c
taaviraudmann <taaviraudmann@gmail.com>
2022-07-10 11:09:12 +0300

    [add] - Person class Lombok annotations which mirrors PersonWithAllSourceCode

---
+++ src/main/java/com/sda/springdemojavaee14/homework/annotation/Person.java
@@ -0,0 +1,17 @@
+package com.sda.springdemojavaee14.homework.annotation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // generates all getters/setter, toString(), equals(), hashcode()
+@AllArgsConstructor // generates constructor for all fields in order of their declaration
+@NoArgsConstructor // generates default constructor
+public class Person {
+
+    private String name;
+
+    private String surname;
+
+    private int age;
+}
