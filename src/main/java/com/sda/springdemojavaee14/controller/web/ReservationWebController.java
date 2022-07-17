88c5e196c3bd4f3d8752cf497eb6f7ebc684391d
taaviraudmann <taaviraudmann@gmail.com>
2022-07-17 12:11:17 +0300

    [add] - reservation page in progress

---
+++ src/main/java/com/sda/springdemojavaee14/controller/web/ReservationWebController.java
@@ -0,0 +1,29 @@
+package com.sda.springdemojavaee14.controller.web;
+
+import com.sda.springdemojavaee14.service.ReservationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/web")
+
+public class ReservationWebController {
+
+    private final ReservationService reservationService;
+
+    public ReservationWebController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+    @GetMapping("/reservations/{id}")
+    public String showReservationById(@PathVariable ("id") Long id, Model data){
+       var reservationFromDB = reservationService.findReservationById(id);
+       data.addAttribute("reservation", reservationFromDB);
+
+       return "reservation";
+    }
+}
