301646b63b0897a23161263e99019debc52b5911
taaviraudmann <taaviraudmann@gmail.com>
2022-07-30 11:21:26 +0300

    [add] - back link from details to all reservation page

--- src/main/java/com/sda/springdemojavaee14/controller/web/ReservationWebController.java
+++ src/main/java/com/sda/springdemojavaee14/controller/web/ReservationWebController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-@RequestMapping("/web/reservations/")
+@RequestMapping("/web/reservations")
 
 public class ReservationWebController {
 
